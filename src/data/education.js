84fc03e63cdf11bad82fbaa2dd55849143038159
Shankar Kumar Nanda <sknandabtech22@vssut.ac.in>
2024-06-11 17:24:54 +0530

    Changes Pushed

--- src/data/education.js
+++ src/data/education.js
@@ -2,57 +2,57 @@ const information = [
 		{
 			"id": 1,
 			"title": "HTML5",
-			"imageSrc": "/public/assets/html.png"
+			"imageSrc": "/assets/html.png"
 		},
 		{
 			"id": 2,
 			"title": "CSS/SCSS",
-			"imageSrc": "public/assets/css.png"
+			"imageSrc": "/assets/css.png"
 		},
 		{
 			"id": 3,
 			"title": "JavaScript",
-			"imageSrc": "public/assets/javascript.png"
+			"imageSrc": "/assets/javascript.png"
 		},
 		{
 			"id": 4,
 			"title": "React",
-			"imageSrc": "public/assets/react.png"
+			"imageSrc": "/assets/react.png"
 		},
 		{
 			"id": 5,
 			"title": "Node.js",
-			"imageSrc": "public/assets/node.png"
+			"imageSrc": "/assets/node.png"
 		},
 		{
 			"id": 6,
 			"title": "Firebase",
-			"imageSrc": "public/assets/firebase.png"
+			"imageSrc": "/assets/firebase.png"
 		},
 		{
 			"id": 7,
 			"title": "Bootstrap",
-			"imageSrc": "public/assets/bootstrap.png"
+			"imageSrc": "/assets/bootstrap.png"
 		},
 		{
 			"id": 8,
 			"title": "Wordpress",
-			"imageSrc": "public/assets/wordpress.png"
+			"imageSrc": "/assets/wordpress.png"
 		},
 		{
 			"id": 9,
 			"title": "Java",
-			"imageSrc": "public/assets/java.png"
+			"imageSrc": "/assets/java.png"
 		},
 		{
 			"id": 10,
 			"title": "Flutter",
-			"imageSrc": "public/assets/flutter.png"
+			"imageSrc": "/assets/flutter.png"
 		},
 		{
 			"id": 11,
 			"title": "Figma",
-			"imageSrc": "public/assets/figma.png"
+			"imageSrc": "/assets/figma.png"
 		}
 	]
 
