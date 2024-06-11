dcfdd347911ceed1500e7e669bbc7001cf2a063c
Shankar Kumar Nanda <sknandabtech22@vssut.ac.in>
2024-06-11 23:57:01 +0530

    Changes Done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import avatar from "../../../public/avatar/avatar.png";
+import avatar from "../../../public/avatar/avatar.jpg";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 import "./home.css";
 import Shapes from "./Shapes";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Home = () => {
 	return (
@@ -13,12 +14,15 @@ const Home = () => {
 				<h1 className='home__name'>Susie Ayala</h1>
 				<span className='home__education'>Full Stack Web Developer</span>
 				<HeaderSocials />
-				<a href='#contact' className='btn'>
+				{/* <a href='#contact' className='btn'>
 					Contact me!
-				</a>
+				</a> */}
 				<ScrollDown />
 			</div>
 			<Shapes />
+			<a href="https://wa.me/447511943208" className="whatsapp-icon" target="_blank" rel="noopener noreferrer">
+				<FaWhatsapp />
+			</a>
 		</section>
 	);
 };
